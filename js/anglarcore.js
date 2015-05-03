89512ad38583c087c72f58cfa3396dda1debe753
Muhammed Athimannil <hello@athimannil.com>
2015-05-03 15:10:57 +0530

    AngularJS with inactive button

---
+++ js/anglarcore.js
@@ -0,0 +1,34 @@
+(function(){
+	var app = angular.module('keywordApp', []);
+	/*app.controller('keywordController', function($scope){
+		$scope.related = [];
+		$scope.categories = [
+			{id: "1", name: "home" },
+			{id: "2", name: "office" },
+			{id: "3", name: "mangaTholi" },
+			{id: "4", name: "ThengaKola" }		
+		];	
+	});*/
+
+	app.controller('keywordController', function($scope, $http) {
+		$scope.selected = [];
+		$scope.selectedmode = null;
+		$http.get('./data.json').then(function(res){
+			$scope.categories = res.data.categories;                
+        });
+
+		$scope.movetorelated = function(section, $index) {
+		   $scope.selected.push(section);
+
+		   // $scope.selectedmode = $index;
+
+	        var e = $scope.categories[$index];
+	        e.selected = !e.selected;
+		   console.log(e.selected);
+		};
+		/*$scope.isSelected = function(section) {
+		    return $scope.selected === section;
+		};*/
+	});
+
+})();
