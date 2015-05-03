715dffdd65c0d967a8ebc26ccdfd70f7ed4cfcf0
Sanoob Pattanath <pshanoop@gmail.com>
2015-05-03 08:05:05 +0530

    Render left side

--- js/script.js
+++ js/script.js
@@ -6,37 +6,37 @@ $(document).ready(function() {
 
 		$.getJSON('./data.json', function(data) {
 		
-			renderCatgories(data);// Render categories with data.json				
-		
+			renderCatgories(data);// Render categories with data.json		
 		});						
 
 		function renderCatgories(data){
 
 			var div 		= $('<div/>',{class:'keyword-item'});
-			var u 	 		= $('<u/>',{class:'pull-right'});
-			var button 	= $('<button/>',{class:'btn btn-defualt btn-xs'})
-			var span 		= $('<span/>',{class:'glyphicon glyphicon-chevron-left'})
+			var u 	 		= $('<u/>',{class:'pull-right',text:'keywords'});
+			var button 		= $('<button/>',{class:'btn btn-defualt btn-xss btn-arrow pull-left'});
+			var span		= $('<span/>',{class:'keyword'});
+			var spanLeft	= $('<span/>',{class:'glyphicon glyphicon-left-arrows'});
+			var p 			= $('<p/>');
 		 
-
 			var cats = data.categories;
 
 			for (var i = 0; i < cats.length; i++) {				 
 			
 				var clnBtn 	= button.clone();
-				var clnDiv		= div.clone();
+				var clnDiv	= div.clone();
+				var clnP	= p.clone();
 
 				clnDiv.data('id', cats[i].id)
 				
-				span.clone().appendTo(clnBtn);
- 				span.clone().appendTo(clnBtn);
+				spanLeft.clone().appendTo(clnBtn);
+
+				clnBtn.appendTo(clnP);
+				span.clone().text(cats[i].name).appendTo(clnP);
 
  				u.clone().appendTo(clnDiv);
- 				clnBtn.appendTo(clnDiv);
- 				
- 				clnDiv.text(cats[i].name)
-
+ 				clnP.appendTo(clnDiv);
+ 			
  				clnDiv.appendTo('#keyword-sel');
-
 			};	
 		}
 	}
