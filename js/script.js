76c20b1fbec158093ad0ef5fda7c959f0ef528e4
Sanoob Pattanath <pshanoop@gmail.com>
2015-05-03 10:39:15 +0530

    JS for interaction

--- js/script.js
+++ js/script.js
@@ -4,16 +4,23 @@ $(document).ready(function() {
 
 	function loadData () {
 
+
+		//Hide category panel
+		$('.btn-hide').click(function(ev) {
+
+		});
+
 		$.getJSON('./data.json', function(data) {
 		
 			renderCatgories(data);// Render categories with data.json		
-		});						
+		});
 
-		function renderCatgories(data){
+		function renderCatgories(data)
+		{
 
 			var div 		= $('<div/>',{class:'keyword-item'});
 			var u 	 		= $('<u/>',{class:'pull-right',text:'keywords'});
-			var button 		= $('<button/>',{class:'btn btn-defualt btn-xss btn-arrow pull-left'});
+			var button 		= $('<button/>',{class:'btn btn-default btn-xss btn-arrow pull-left btn-left'});
 			var span		= $('<span/>',{class:'keyword'});
 			var spanLeft	= $('<span/>',{class:'glyphicon glyphicon-left-arrows'});
 			var p 			= $('<p/>');
@@ -26,7 +33,8 @@ $(document).ready(function() {
 				var clnDiv	= div.clone();
 				var clnP	= p.clone();
 
-				clnDiv.data('id', cats[i].id)
+				clnBtn.data('id', cats[i].id);
+				clnBtn.data('name', cats[i].name);
 				
 				spanLeft.clone().appendTo(clnBtn);
 
@@ -37,7 +45,49 @@ $(document).ready(function() {
  				clnP.appendTo(clnDiv);
  			
  				clnDiv.appendTo('#keyword-sel');
-			};	
+			}
 		}
+		
+
+		//Add category 
+
+		$('#keyword-sel').on('click','.btn-left',(function(ev) {
+			var dataId 		= $(this).data('id');
+			var dataName	= $(this).data('name');
+			
+
+			//add to left					
+			$('#keyword-wrap').append(addCategory(dataId,dataName));
+
+			//Disable Item to avoid duplicate entry  
+
+			$(this).addClass('disabled');
+
+		}));
+
+		function addCategory(id,name)
+		{
+			var div 	= $('<div/>',{class:'keyword-item'});
+			var u 		= $('<u/>',{class:'pull-right',text:'keywords'});
+			var label	= $('<label/>').html('<input class="btn-select" type="checkbox">');
+			var span 	= $('<span/>',{class:'glyphicon',text:name});
+
+			div.data('id', id);
+			div.data('name', name);
+
+			u.appendTo(div);
+			span.appendTo(label);
+			label.appendTo(div);
+
+			return div;			
+		}
+
+
+		//Checkbox checked
+
+		$('#keyword-wrap').on('click','.btn-select',function(ev){
+			$(this).parents('.keyword-item').data('selected',$(this).is(':checked'));
+		});
+
 	}
 });
